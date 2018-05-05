511fabd341bcb79633b2a57444b3f9aea2b292d5
shandy <shandysaputra00@gmail.com>
2018-05-05 09:41:41 +0800

    method dengan nilai baik

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02;
-
-/**
- *
- * @author shandy
- */
-public class rectangle {
+public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("===============");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar : "+lebar);
+        System.out.println("===============");
+    }
+      double hitungLuas(){
+        double luas;
+        luas=panjang*lebar;
+        return luas;
+    }
 }
